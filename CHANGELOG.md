0489f2cb21a3c914a48b9b18a696f0c52ffc2344
etki <etki@etki.me>
2019-03-10 19:57:30 +0300

    Version 0.1.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.9] - 10-03-2019
+
+- Changed allure report directory to {root}/target/allure/report
+- Added license files
+
 ## [0.1.8] - 10-03-2019
 
 - Added junit params dependency
--- src/validator/src/test/java/io/ayte/twill/validator/MultiplierTest.java
+++ src/validator/src/test/java/io/ayte/twill/validator/MultiplierTest.java
@@ -1,6 +1,8 @@
 package io.ayte.twill.validator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -14,5 +16,18 @@ public class MultiplierTest {
     @Test
     public void validateMultiplication() {
         assertThat(new Multiplier(21).apply(2), equalTo(42));
+    }
+
+    public static Object[][] dataProvider() {
+        return new Object[][] {
+                {1, 2, 2},
+                {3, 4, 12}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void validateParameterizedMultiplication(int leftHand, int rightHand, int expectation) {
+        assertThat(new Multiplier(leftHand).apply(rightHand), equalTo(expectation));
     }
 }
